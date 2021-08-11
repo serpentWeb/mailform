818d20e4d4b06bf1878a901736b93025b87f2c0f
Roman Weber <proto4el@mail.ru>
2021-08-12 00:12:54 +0200

    правка2

--- index.js
+++ index.js
@@ -7,16 +7,14 @@ const port = 3000;
 
 const https = require('https');
 const fs = require('fs');
-const port = 3000;
 
-var key = fs.readFileSync(__dirname + '/../selfsigned.key');
-var cert = fs.readFileSync(__dirname + '/../selfsigned.crt');
+var key = fs.readFileSync(__dirname + '/selfsigned.key');
+var cert = fs.readFileSync(__dirname + '/selfsigned.crt');
 var options = {
   key: key,
   cert: cert
 };
 
-app = express()
 app.get('/', (req, res) => {
    res.send('Now using https..');
 });
