316a39ea8111618def7d3e932a8a5c86f86470ff
Roman Weber <proto4el@mail.ru>
2021-08-12 00:02:11 +0200

    SSL

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const port = 3000;
 
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const port = 3000;
+
+var key = fs.readFileSync(__dirname + '/../selfsigned.key');
+var cert = fs.readFileSync(__dirname + '/../selfsigned.crt');
+var options = {
+  key: key,
+  cert: cert
+};
+
+app = express()
+app.get('/', (req, res) => {
+   res.send('Now using https..');
+});
+
+var server = https.createServer(options, app);
+
+server.listen(port, () => {
+  console.log("server starting on port : " + port)
+});
+
 class MainController {
   static async sendEmail(req, res) {
     const { name, phoneNumber } = req.query;
